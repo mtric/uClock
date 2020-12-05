764d9f9836142cf5a0f6a64dd562a0d38546ade7
mtric <29257165+mtric@users.noreply.github.com>
2020-12-05 13:03:49 +0100

    Initial Commit

---
+++ MyHours/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyHours
+//
+//  Created by Eric Walter on 05.12.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyHours/MyHoursApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHoursApp.swift
+//  MyHours
+//
+//  Created by Eric Walter on 05.12.20.
+//
+
+import SwiftUI
+
+@main
+struct MyHoursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
